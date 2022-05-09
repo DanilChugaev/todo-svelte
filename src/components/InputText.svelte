b4b9972fb4f36ba79b3cb127718fe04a4d7d9789
danilchugaev <dmchugaev@gmail.com>
2022-05-10 00:03:13 +0700

    add inputtext

---
+++ src/components/InputText.svelte
@@ -0,0 +1,42 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    let value = '';
+
+    /**
+     * Handle enter key from input field
+     *
+     * @param {KeyboardEvent} event
+     * @returns {void}
+     */
+    function handleEnter(event) {
+        if (event.code === 'Enter') {
+            dispatch('enter', {value});
+            clearValue();
+        }
+    }
+
+    /**
+     * Clear text in input field
+     *
+     * @returns {void}
+     */
+    function clearValue() {
+        value = '';
+    }
+</script>
+
+<input
+        class="input-text"
+        type="text"
+        bind:value={value}
+        on:keypress={handleEnter}
+>
+
+<style>
+    .input-text {
+        width: 100%;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import InputText from "../components/InputText.svelte";
+
+    let result = '';
+
+    function handleMessage(event) {
+        result = event.detail.value;
+    }
+</script>
+
+<h1>It's my mini todo</h1>
+
+<div class="wrapper">
+    <InputText on:enter={handleMessage}/>
+
+    {result}
+</div>
+
+<style>
+    .wrapper {
+        width: 100%;
+        max-width: 400px;
+    }
+</style>
